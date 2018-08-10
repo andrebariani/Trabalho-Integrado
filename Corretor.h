2db7a5671bc060354c34e3e71c844735b83ea1e3
andrebariani <andrebariani@gmail.com>
2018-08-09 23:54:38 -0300

    Mudei um metodo errado aqui, sorry

--- Corretor.h
+++ Corretor.h
@@ -19,7 +19,7 @@ class Corretor {
             int contador;
         } erros;
         forward_list<erros> wrongWords;
-        void iniciarCorrecao(); // Inicia processo de correção do arquivo de texto inteiro
+        void corrigir();
         void apresentarErro(Palavra palavraErrada);
         Palavra selecPalavraSemelhante(Palavra palavraErrada);
         void adicionarPalavraDicionario(Palavra palavraErrada);
@@ -29,7 +29,7 @@ class Corretor {
     public:
         Corretor(std::string nometexto);
         // void mudarTexto( string nome_texto );
-        void corrigir();
+        void iniciarCorrecao(); // Inicia processo de correção do arquivo de texto inteiro
     	// void apresentarErro();
     	// void corrigir();// usar uma palavra fornecida pelo dicionário para corrigir uma palavra errada no texto
     	// void ignorar();//ignorar um erro encontrado pelo corretor e não fazer mais nada
