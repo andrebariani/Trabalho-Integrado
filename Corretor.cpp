71724587a51f67efd3d6086ae2744773ed42424d
Anderson Garrote <ander.user@gmail.com>
2018-08-09 23:26:47 -0300

    Coisas deixadas mais bonitinho

--- Corretor.cpp
+++ Corretor.cpp
@@ -21,7 +21,6 @@ void Corretor::iniciarCorrecao() {
 
 
 	while(!(atual == Null)) {
-	 	wcout << "Conferindo palavra: " << atual << endl;
 		if(!dic.buscaPalavra(atual)) {
 			apresentarErro(atual);
 		}
@@ -45,24 +44,28 @@ void Corretor::apresentarErro(Palavra palavraErrada){
 	Palavra pp = texto.getPalavraProximo();
 	int op;
 
-	cout << "Uma palavra inexistente no dicionário foi encontrada no texto: \"...";
-	wcout << pa << " " << palavraErrada << " " << pp << L"...\", por favor selecione uma opção\n";
-	cout << "Opção 1: Inserir a palavra correta\n";
-	cout << "Opção 2: Ignorar o erro\n";
-	cout << "Opção 3: Selecionar palavra semelhante encontrada no dicionário\n";
-	cout << "Opção 4: Adicionar a palavra ao dicionário\n";
+	cout << "Palavra errada encontrada: ";
+	wcout << palavraErrada;
+	cout << "\nContexto:\"...";
+	wcout << pa << " " << palavraErrada << " " << pp << L"...\"\n ";
+	cout << "Por favor selecione uma opção\n";
+	cout << " -Opção 1: Inserir a palavra correta\n";
+	cout << " -Opção 2: Ignorar o erro\n";
+	cout << " -Opção 3: Selecionar palavra semelhante encontrada no dicionário\n";
+	cout << " -Opção 4: Adicionar a palavra ao dicionário\n";
 
 	adicionarErro(palavraErrada);
 
 	do{
 		cout << "Escolha a opcão desejada: " << endl;
+		cout << ">>";
 		cin >> op;
 		if(op == 1){
-				corrigir();
+			corrigir();
 			break;
 		}
 		else if(op == 2){
-				return;
+			return;
 			break;
 		}
 		else if(op == 3){
@@ -81,10 +84,6 @@ void Corretor::apresentarErro(Palavra palavraErrada){
 	}while(op < 1 || op > 4);
 }
 
-// void mudarTexto( string nome_texto ) {
-// 	texto.carregarNovoTexto(nome_texto);
-// }
-
 void Corretor::corrigir(){// usar uma palavra fornecida pelo usuário para corrigir uma palavra errada no texto
 	//permitir que o usuário insira uma palavra e substitua a palavra errada
 	wstring correta;
@@ -93,13 +92,10 @@ void Corretor::corrigir(){// usar uma palavra fornecida pelo usuário para corri
 	wcin >> correta;
 
 	texto.corrigirPalavra(correta);
-
-		//corrigirPalavra - texto.cpp
 }
 
 Palavra Corretor::selecPalavraSemelhante(Palavra palavraErrada){//utilizar palavra semelhante sugerida
 	//substituir palavra errada por palavra semelhante do dicionário
-	// queue<Palavra> iSemelhantes = dic.buscaSemelhante(palavraErrada);
 	queue<Palavra> iSemelhantes;
 
 	iSemelhantes = dic.buscaSemelhante(palavraErrada, iSemelhantes);
@@ -109,12 +105,6 @@ Palavra Corretor::selecPalavraSemelhante(Palavra palavraErrada){//utilizar palav
 		cout << "Palavra semelhante não encontrada no dicionario...\n" << endl;
 		return Null;
 	}
-	// Palavra p;
-	//
-	// while(j--) {
-	// 	wcin >> p;
-	// 	iSemelhantes.push(p);
-	// }
 
 	int size = iSemelhantes.size();
 
@@ -129,6 +119,7 @@ Palavra Corretor::selecPalavraSemelhante(Palavra palavraErrada){//utilizar palav
 		iSemelhantes.push(iSemelhantes.front());
 		iSemelhantes.pop();
 	}
+	std::cout << "\n>>";
 
 	Palavra escolhida;
 
@@ -143,6 +134,7 @@ Palavra Corretor::selecPalavraSemelhante(Palavra palavraErrada){//utilizar palav
 			return escolhida;
 		} else {
 			cout << "Selecione Opção válida" << endl;
+			std::cout << ">>";
 		}
 	}while(op < j+1 && op > 0);
 
@@ -182,129 +174,16 @@ void Corretor::adicionarErro(Palavra palavraErrada){
 }
 
 void Corretor::exibirErros() {
+	std::cout << "Sem mais erros!" << '\n';
+	std::cin.ignore();
+	std::cout << "===============" << '\n';
+	std::cout << "Lista de Erros:" << '\n';
 	std::forward_list<erros>::iterator it;
 	wrongWords.reverse();
 	for(it = wrongWords.begin() ; it != wrongWords.end() ; it++){
 		wcout << it -> palavra << ": " << L"Errou " << it -> contador << L" vez(es)" << endl;
 	}
-
+	std::cin.ignore();
 	while(!wrongWords.empty())
 		wrongWords.pop_front();
 }
-
-
-//
-//
-//
-// // OLD VERSION
-// //lista das palavras erradas
-// 	forward_list<erros> wrongWords;
-//
-// //main(){
-// 	//utilizar funções:
-// 	//		percorrerTexto - Texto.cpp
-// 	//		consulta - dicionario.cpp
-// 	// try catch
-// //}
-//
-// 	void Corretor::adicionarErro(Palavra palavraErrada){
-// 		//se palavra não encontrada no dicionário, adicionar erro na lista
-// 		//caso mesmo erro ocorra novamente, aumentar contagem
-// 		//apresentarErro
-//
-//
-// 		if(wrongWords.empty()){
-// 			erros r;
-// 			r.palavra = P;
-// 			r.contador = r.contador + 1;
-// 			wrongWords.assign(r);
-// 		}
-// 		else{
-// 			for(int i=wrongWords.begin(); i != wrongWords.end(); i++){
-// 				if(r.palavra == P){
-// 					r.contador = r.contador + 1;
-// 				}
-// 				else if((r.palavra != P) && ((i+1)==wrongWords.end())) {
-// 				erros r;
-// 				r.palavra = P;
-// 				r.contador = r.contador + 1;
-// 				wrongWords.push_front(r)
-// 				}
-// 			}
-// 		}
-// 	}
-//
-//
-//
-//
-// 	void Corretor::apresentarErro(Palavra palavraErrada){
-// 		//apresentar contexto
-// 		//oferecer opções
-// 		//utilizar função: contexto - texto.cpp
-// 		contexto();
-// 		int op;
-//
-// 		cout << "Uma palavra inexistente no dicionário foi encontrada no texto: \"..." <<tex.getPalavraAnterior << palavraErrada << tex.getPalavraProximo <<"...\", por favor selecione uma opção\n";
-// 		cout << "Opção 1: Inserir a palavra correta\n"
-// 		cout << "Opção 2: Ignorar o erro\n"
-// 		cout << "Opção 3: Selecionar palavra semelhante encontrada no dicionário\n"
-// 		cout << "Opção 4: Adicionar a palavra ao dicionário\n"
-//
-// 			cin >> op;
-// 		do{
-// 			if(op == 1){
-// 				corrigir();
-// 				break;
-// 			}
-// 			else if(op == 2){
-// 				ignorarErro();
-// 				break;
-// 			}
-// 			else if(op == 3){
-// 				selecPalavraSemelhante();
-// 				break;
-// 			}
-// 			else if(op == 4){
-// 				adicionarDicionario();
-// 				break;
-// 			}
-// 			else{
-// 				cout << "Opção inválida";
-// 				cin >> op;
-// 			}
-// 		}while(op < 1 || op > 4);
-//
-//
-// 	}
-//
-// 	//opções do corretor
-// 	void Corretor::corrigir(){// usar uma palavra fornecida pelo usuário para corrigir uma palavra errada no texto
-// 		//permitir que o usuário insira uma palavra e substitua a palavra errada
-// 		string correta;
-// 		cout << "Insira a palavra correta";
-// 		cin >> correta;
-//
-// 		tex.corrigirPalavra(correta);
-//
-// 			//corrigirPalavra - texto.cpp
-// 	}
-//
-// 	int Corretor::ignorarErro(){//ignorar um erro encontrado pelo corretor e não fazer mais nada
-// 		return 0;
-// 	}
-//
-// 	Palavra Corretor::selecPalavraSemelhante(Palavra p){//utilizar palavra semelhante sugerida
-// 		//substituir palavra errada por palavra semelhante do dicionário
-// 		Palavra *iSemelhantes;
-// 		int j;
-//
-// 		iSemelhantes = dic.PalavraSemelhante(p);
-// 		for(j = 1; j < 6; j++){
-// 			cout << j << "-" << *iSemelhantes << endl;
-// 			iSemelhantes++;
-// 		}
-// 	}
-//
-// 	void Corretor::adicionarDicionario(Palavra palavraErrada){
-// 		//classe inclusão - Dicionario.cpp
-// 	}
